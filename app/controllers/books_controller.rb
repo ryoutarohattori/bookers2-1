7123b4b5388c8605f9a83d4ec4fbe56acf173011
EC2 Default User <ec2-user@ip-172-31-9-19.ap-northeast-1.compute.internal>
2023-03-19 14:21:34 +0000

    [Update]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,26 +2,29 @@ class BooksController < ApplicationController
   def new
    @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to '/books/:id'
   end
 
   def index
-    
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @books = Book.all
   end
 
   def edit
+    
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_17_085139) do
+ActiveRecord::Schema.define(version: 2023_03_19_080902) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -41,11 +41,11 @@ ActiveRecord::Schema.define(version: 2023_03_17_085139) do
   end
 
   create_table "books", force: :cascade do |t|
-    t.string "name"
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
